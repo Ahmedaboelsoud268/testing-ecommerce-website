0aa0ab2edcc4712689ea6911615416e828f7bd55
ahmed maghraby <ahmedaboelsoud285@gmail.com>
2024-10-22 03:21:06 +0300

    the login feature

---
+++ project_ecommerce/src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -0,0 +1,9 @@
+package org.example.stepDefs;
+import io.cucumber.java.en.Given;
+public class D01_registerStepDef {
+    @Given("user go to register page")
+    public void user_go_to_register_page()
+    {
+        System.out.println("welcome in the projects");
+    }
+}
---
+++ project_ecommerce/src/test/java/org/example/stepDefs/Hooks.java
@@ -0,0 +1,26 @@
+package org.example.stepDefs;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Hooks {
+    public static WebDriver driver;
+    @Before
+    public static void OpenBrowser()
+    {
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        //navigate to url
+        driver.get("https://demo.nopcommerce.com/");
+    }
+    @After
+    public static void quitDriver() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
